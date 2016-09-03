bb82f13cbf637affb0644e3d99418930dfc69097
Heather Park <hthr.prk@gmail.com>
2016-09-02 23:37:38 -0700

    Implement functions for front-end's core logic

---
+++ src/core.js
@@ -0,0 +1,33 @@
+import { List } from 'immutable';
+
+export function setCurrentShows(state, shows) {
+  return state.set('currentShows', List(shows));
+}
+
+export function addShow(state, show) {
+  return state.update('currentShows', currentShows => currentShows.push(show));
+}
+
+export function removeShow(state, show) {
+  var currentShows = state
+    .get('currentShows')
+    .filterNot(currentShow => currentShow.get('name') === show.get('name'));
+
+  return state.set('currentShows', currentShows);
+}
+
+export function toggleWatched(state, show, episodeId) {
+  var showToUpdate = state
+    .get('currentShows')
+    .findIndex(targetShow => targetShow.get('name') === show.get('name'));
+
+  var episodeToUpdate = state
+    .getIn(['currentShows', showToUpdate, 'episodes'])
+    .findIndex(targetEpisode => targetEpisode.get('id') === episodeId);
+
+  return state
+    .updateIn(
+      ['currentShows', showToUpdate, 'episodes', episodeToUpdate, 'watched'],
+      watched => !watched
+    );
+}
