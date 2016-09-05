b1e4e3169fd13092e8097b7187aec1e7dede33db
Heather Park <hthr.prk@gmail.com>
2016-09-05 16:50:21 -0700

    Update CurrentShow component to render more show information

--- src/components/CurrentShow.jsx
+++ src/components/CurrentShow.jsx
@@ -8,17 +8,15 @@ export default React.createClass({
   mixins: [PureRenderMixin],
 
   render() {
-    const { show } = this.props;
-    const id = show.get('_id');
+    let { show } = this.props
+    let id = show.get('_id')
 
     return (
       <div className="current-show">
         <h1>{show.get('name')}</h1>
+        <p>{show.get('network')}</p>
+        <p>{show.get('firstAired')}</p>
         <p>{show.get('overview')}</p>
-        <button>Episodes</button>
-        <button
-          className="remove-option"
-          onClick={() => store.dispatch(toggleWatched(id))}>Remove Show</button>
       </div>
     )
   }
