6a73b747706d80dbee4ea4bf598dfb64b4f6cf36
Heather Park <hthr.prk@gmail.com>
2016-09-08 13:23:21 -0700

    Update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="add-show-container">
+    <header>
       <h1>drama queen!</h1>
-    </div>
+      <p className="description"> This is a personal app to keep track of the Korean dramas (and my ratings for them) that I've watched.</p>
+      <p><a href="https://github.com/heatherpark/dramaqueen" target="_blank">check out the code on github</a></p>
+    </header>
   )
 }
 
